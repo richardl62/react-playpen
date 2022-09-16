e4b1115ae4598b21003ce363034e34a9c8e76e5b
Richard L <richard.latham@yahoo.co.uk>
2022-09-16 11:42:23 +0100

    Wip on arcs

--- src/score-board.tsx
+++ src/score-board.tsx
@@ -5,7 +5,10 @@ const height = 600;
 const width = 200;
 
 const radius = 10;
-const gap = radius/2;
+const standardGap = radius/2;
+const fifthRowGap = radius;
+
+const arcRadius = 60;
 
 const Board = styled.div`
     position: relative;
@@ -37,17 +40,49 @@ export function ScoreBoard() {
         holes.push(<Hole pos={[bottom, left]} />);
     }
 
-    const addColumn = (colBottom: number, left: number, size: number) => {
-        for(let ind = 0; ind < size; ++ind) {
-            const holeBottom = colBottom + ind * (radius+gap);
-            addHole(holeBottom, left);
-            addHole(holeBottom, left+radius+gap);
-        }
+    const addArcHole = (center: number[], radius: number, angle: number) => {
+        const [bottom, left] = center;
+
+        const bottomOffset = Math.sin(angle);
+        const leftOffset = -Math.cos(angle);
+        addHole(
+            bottom + bottomOffset * radius,
+            left + leftOffset * radius,
+            )
     }
 
-    addColumn(0, 0, 30);
-    addColumn(0, 40, 30);
-    addColumn(0, 80, 30);
+    const addColumn = (pos: number[], count: number, direction: "up" | "down") => {
+        let [bottom, left] = pos;
+
+        for(let ind = 1; ind <= count; ++ind) { // Count from 1
+            addHole(bottom, left);
+            addHole(bottom, left+radius+standardGap);
+
+            const sign = direction === "up" ? 1 : -1;
+            const gap = (ind % 5 === 0) ? fifthRowGap : standardGap;
+            bottom += sign * (radius + gap);
+        }
+
+        return [bottom, left];
+    }
+
+    const addArc = (pos: number[]) => {
+        let center = [...pos];
+        center[1] += arcRadius;
+
+        const nSteps = 10;
+        for(let ind = 0; ind < nSteps; ++ind) {
+            const angle = Math.PI * ind / (nSteps-1);
+
+            addArcHole(center, arcRadius, angle); 
+            addArcHole(center, arcRadius - radius - standardGap, angle); 
+        }
+    };
+
+    let pos = [standardGap, standardGap];
+    //pos = addColumn(pos, 20, "up");
+    addArc(pos)
+    //addColumn([pos[0], pos[1]+50], 20, "down")
 
     return <Board>{holes}</Board>;
 }
