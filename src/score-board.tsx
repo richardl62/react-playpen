595c5a11b8ce31e41bddd7e2f1f92b422661a5aa
Richard L <richard.latham@yahoo.co.uk>
2022-09-16 12:50:48 +0100

    More work on arcs

--- src/score-board.tsx
+++ src/score-board.tsx
@@ -10,6 +10,11 @@ const fifthRowGap = radius;
 
 const arcRadius = 60;
 
+interface Position {
+    bottom: number;
+    left: number;
+}
+
 const Board = styled.div`
     position: relative;
 
@@ -20,15 +25,15 @@ const Board = styled.div`
     margin: 10px;
 `;
 
-const Hole = styled.div<{pos: [number, number]}>`
+const Hole = styled.div<{pos: Position}>`
 
     height: ${radius}px;
     width: ${radius}px;
     border-radius: 50%;
 
     position: absolute; 
-    bottom: ${props => props.pos[0]}px;
-    left:${props => props.pos[1]}px;;
+    bottom: ${props => props.pos.bottom}px;
+    left:${props => props.pos.left}px;;
 
     background: black;
 `
@@ -36,53 +41,60 @@ const Hole = styled.div<{pos: [number, number]}>`
 export function ScoreBoard() {
     const holes : JSX.Element [] = [];
 
-    const addHole = (bottom: number, left: number) => {
-        holes.push(<Hole pos={[bottom, left]} />);
+    const addHole = (pos: Position) => {
+        holes.push(<Hole pos={pos} />);
     }
 
-    const addArcHole = (center: number[], radius: number, angle: number) => {
-        const [bottom, left] = center;
+    const addArcHole = (center: Position, radius: number, angle: number) => {
+        let {bottom, left} = center;
 
-        const bottomOffset = Math.sin(angle);
-        const leftOffset = -Math.cos(angle);
-        addHole(
-            bottom + bottomOffset * radius,
-            left + leftOffset * radius,
-            )
+        bottom += Math.sin(angle) * radius;
+        left += -Math.cos(angle) * radius;
+        addHole({bottom, left});
     }
 
-    const addColumn = (pos: number[], count: number, direction: "up" | "down") => {
-        let [bottom, left] = pos;
+    const addColumn = (pos: Position, count: number, direction: "up" | "down") => {
+        let {bottom, left} = pos;
 
         for(let ind = 1; ind <= count; ++ind) { // Count from 1
-            addHole(bottom, left);
-            addHole(bottom, left+radius+standardGap);
+            addHole({bottom, left});
+            addHole({bottom, left: left+radius+standardGap});
 
             const sign = direction === "up" ? 1 : -1;
             const gap = (ind % 5 === 0) ? fifthRowGap : standardGap;
             bottom += sign * (radius + gap);
         }
 
-        return [bottom, left];
+        return {bottom, left};
     }
 
-    const addArc = (pos: number[]) => {
-        let center = [...pos];
-        center[1] += arcRadius;
+    const addSimpleArc = (center: Position, 
+            span: {startAngle: number, endAngle: number, steps: number}
+        ) => {
+        const startRad = span.startAngle * (Math.PI/180);
+        const endRad = span.endAngle * (Math.PI/180);
 
-        const nSteps = 10;
-        for(let ind = 0; ind < nSteps; ++ind) {
-            const angle = Math.PI * ind / (nSteps-1);
+        const step = (endRad-startRad) / (span.steps-1);
 
-            addArcHole(center, arcRadius, angle); 
-            addArcHole(center, arcRadius - radius - standardGap, angle); 
+        for(let ind = 0; ind < span.steps; ++ind) {
+            const rad = startRad + step * ind;
+
+            addArcHole(center, arcRadius, rad); 
+            addArcHole(center, arcRadius - radius - standardGap, rad); 
         }
     };
 
-    let pos = [standardGap, standardGap];
-    //pos = addColumn(pos, 20, "up");
-    addArc(pos)
-    //addColumn([pos[0], pos[1]+50], 20, "down")
+    const add10StepArc = (start: Position) => {
+        let center = {...start};
+        center.left += arcRadius;
+
+        addSimpleArc(center, {startAngle:0, endAngle:75, steps: 5});
+        addSimpleArc(center, {startAngle:105, endAngle:180, steps:5});       
+    };
+
+    let start = {bottom:standardGap, left:standardGap};
+    start = addColumn(start, 30, "up");
+    add10StepArc(start);
 
     return <Board>{holes}</Board>;
 }
