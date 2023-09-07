883d823e1fc00b85eb33ac7892cf4d5a936b3b9a
Richard L <richard.latham@yahoo.co.uk>
2023-09-07 18:32:44 +0100

    Wip on getting word definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,46 @@
 import React, { useState } from 'react';
 import './App.css';
-import styled from "styled-components";
 
-const OuterDiv = styled.div`
-  display: flex;
-  position: relative;
-`;
 
-const Number = styled.div<{selected: boolean, index: number}>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const DICTIONARY_API_BASE_URL =
+    'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
-  position: relative;
-  left: ${props => props.index * -20}px;
-  z-index: ${props => props.index}; // Not strictly necessary
+const fetchWordDefinitions = async (word: string) => {
+    console.log(`Making request for definitions of ${word}...`);
 
-  height: 120px;
-  width: 80px;
-  border-radius: 20%;
+    const response = await fetch(DICTIONARY_API_BASE_URL + word);
+    const json = await response.json();
+    console.log(json)
+    return Array.isArray(json) ? "found" : "not found";
+};
 
-  font-size: 80px;
-
-  color: ${props => props.selected ? "red" : "black"};
-  border: 4px solid ${props => props.selected ? "red" : "black"};
-
-  background-color: white;
-
-`
 function App() {
-  const [selected, setSelected] = useState<boolean[]>([]);
+  const [word, setWord] = useState("");
+  const [definition, setDefinition] = useState("");
 
-  const toggleSelected = (index: number) => {
-     const newSelected = [...selected];
-     newSelected[index] = !newSelected[index];
-     setSelected(newSelected);
+  const onCheckWord = () => {
+    setDefinition("...")
+
+    fetchWordDefinitions(word).then(defintions => 
+      setDefinition(word + defintions)
+    )
+    .catch(_ => {
+        setDefinition(`Could not retrive any defintions for ${word}`)
+    });
   }
+  return <div>
+    <div>
+      <input
+        value={word}
+        onChange={event => setWord(event.target.value)}
+      />
+      <button onClick={onCheckWord}>
+          Check word
+      </button>
+    </div>
 
-  const elems : JSX.Element[] = [];
+    <div>{definition}</div>
 
-  for (let i = 0; i < 10; ++i) {
-    elems.push(<Number 
-      selected={selected[i]} 
-      index={i}
-
-      key={i}
-      onClick={()=>toggleSelected(i)}>
-      {i}
-    </Number>);
-  }
-
-  return <OuterDiv>{elems}</OuterDiv>
+    </div>
 }
-
 export default App;
